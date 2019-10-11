9dded6be396183bb43cf929a6a3541f3188f233a
aguiarpo <Sk4t3l1f342-+>
2019-10-11 16:38:58 -0300

    Adicionado ColorsUsed class

---
+++ lib/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorsUsed {
+  static const Color mainColor = Color(0xff8CFFBA);
+  static const Color greenDarkColor = Color(0xff1A773F);
+  static const Color secundaryColor = Color(0xff66D994);
+  static const Color terciaryColor = Color(0xff4F816D);
+  static const Color blueColor = Color(0xff179CBF);
+  static const Color blueDarkColor = Color(0xff2C1871);
+}
