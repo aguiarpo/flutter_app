9fed609b873abaafc91154ef6bc2470fc50451ab
aguiarpo <Sk4t3l1f342-+>
2019-10-11 16:39:24 -0300

    Adicionado novas rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/config.dart';
+import 'package:flutter_app/pages/edit_cont.dart';
 import 'package:flutter_app/pages/home.dart';
 import 'package:flutter_app/pages/login.dart';
+import 'package:flutter_app/pages/medications/edit.dart';
 import 'package:flutter_app/pages/register.dart';
 import 'package:flutter_app/pages/reset_password.dart';
 
@@ -19,6 +21,8 @@ void main() => runApp(MaterialApp(
     '/config' : (context) => ConfigPage(),
     '/resetPassword1' : (context) => ResetPassword(),
     '/register' : (context) => Register(),
+    '/editCont' : (context) => EditCont(),
+    '/editMedications' : (context) => EditMedications(),
   },
 )
 );
