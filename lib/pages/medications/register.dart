aa8f5d4b51729d79a2b7f70d568c4a0deea36318
aguiarpo <Sk4t3l1f342-+>
2019-10-14 17:46:06 -0300

    Adicionado RegisterMedications class

---
+++ lib/pages/medications/register.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/components/my_button.dart';
+import 'package:flutter_app/components/my_text_field.dart';
+import 'package:flutter_app/icons/surca_icons.dart';
+import 'package:flutter_app/colors.dart';
+
+class RegisterMedications extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: ColorsUsed.greenDarkColor, //change your color here
+        ),
+        title: Text("Editar", style: TextStyle(
+          fontFamily: 'Roboto', fontWeight: FontWeight.w400,
+          color: ColorsUsed.greenDarkColor,
+        ),
+        ),
+        backgroundColor: ColorsUsed.mainColor,
+      ),
+      body: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              MyTextField(
+                icon: Surca.vaccine,
+                hint: "Nome",
+              ),
+              Padding(
+                padding: EdgeInsets.only(top : 30, left: 100, right: 100),
+                child: MyButton(
+                  text: "Cadastrar",
+                  onPress: (){Navigator.pop(context);},
+                ),
+              )
+            ],
+          )
+      ),
+    );
+  }
+}
